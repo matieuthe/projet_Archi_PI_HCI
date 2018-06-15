1ae91f45775567e07bcede30f612093a713475b2
Mathieu THE <matieuthe@gmail.com>
2018-06-15 07:49:24 +0700

    Modification fonction calcul consommation

--- process/calcConso.php
+++ process/calcConso.php
@@ -8,39 +8,54 @@ while($r = mysqli_fetch_assoc($result)){
     $debit = $r['value']; //debit du robinet à la minute
 }
 
-$selectsql = "SELECT * FROM tap_event WHERE MONTH(recordTime) = 6 AND YEAR(recordTime) = 2018 order by recordTime";
+$selectsql = "SELECT * FROM tap_event WHERE MONTH(recordTime) = 06 AND YEAR(recordTime) = 2018 order by recordTime";
 $record = array();
 $result = mysqli_query($con, $selectsql);
 while($r = mysqli_fetch_assoc($result)){
     $record[] = $r; //Event occuring in a month
 }
 
-$consoMonth = 0;
 $consoJournal = array();
-$compteurDay=0;
 $consoJournal[0] = 0;
+
+$compteurDay=0;
+$consoMonth = 0;
+
 foreach($record as $temp){
+    //Event of opening the tap
     if($temp['type'] == 1){
         $dateTemp = new DateTime($temp['recordTime']);
         if(isset($dateClose)){
-            if($dateClose->format('Y-m-d') == $dateTemp->format('Y-m-d'))
-                $consoJournal[++$compteurDay] = 0;
-        }
-    }else{
-        $dateClose = new DateTime($temp['recordTime']);
-        if(isset($dateTemp)){
             if($dateClose->format('Y-m-d') != $dateTemp->format('Y-m-d')){
-                echo $dateClose->format('Y-m-d')." :: ".$dateTemp->format('Y-m-d')."<br>";
                 $consoJournal[++$compteurDay] = 0;
             }
         }
-        $diff = abs($dateTemp->getTimestamp() - $dateClose->getTimestamp())/60;
-        $consoMonth += $diff * $debit;
-        $consoJournal[$compteurDay] += $diff * $debit;
+    }
+    //Event of closing the tap
+    else{
+        $dateClose = new DateTime($temp['recordTime']);
+        if(isset($dateTemp)){
+            if($dateClose->format('Y-m-d') != $dateTemp->format('Y-m-d')){
+                //Total time between the two events
+                $diffTotal = abs($dateTemp->getTimestamp() - $dateClose->getTimestamp())/60;
+                $startDay = new DateTime($dateClose->format('Y-m-d'));
+                $diffStart = abs($startDay->getTimestamp() - $dateClose->getTimestamp())/60;
+                //Add conso to day before
+                $consoJournal[$compteurDay] += ($diffTotal - $diffStart) * $debit;
+                //Add the rest to the day after
+                $consoJournal[++$compteurDay] = $diffStart * $debit;
+            }else{
+                $diff = abs($dateTemp->getTimestamp() - $dateClose->getTimestamp())/60;
+                $consoJournal[$compteurDay] += $diff * $debit;
+            }
+        }else{//If the record start by a closing event
+            $startday = $dateClose->format('Y-m-d');
+            $diffStart = abs($startDay->getTimestamp() - $dateClose->getTimestamp())/60;
+            $consoJournal[$compteurDay] += $diffStart * $debit;
+        }
     }
 }
 
-echo round($consoMonth/1000,1)."m²";
 echo "$compteurDay<br><br>";
 foreach($consoJournal as $temp){
     echo $temp."<br>";
